cbd8efb5d84da858aefb1c81687ce95780457500
Jonathan Lee <Jonwlee457@gmail.com>
2015-09-06 05:18:22 -0400

    added transitions
    add more to the array of transitions later

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,35 +25,21 @@ function myCallback(e) {
 
     document.getElementById("words_total").value = responseXML.words_total; // words total
 
+    var array =  ["including","indeed","further", "either","neither","also","moreover","furthermore","actually",	
+		"additionally","alternatively"];
+		var trans_total=0;
 
- //      var opa1 = 1- responseXML.panel1 ;
- //    var opa2 =  1-responseXML.panel2 ;
-
-	// var opa3 = 1- responseXML.panel3 ;
-
-	// var opa4 =  1-responseXML.panel4 ;
-
-	// var opa5 =  1-responseXML.panel5 ;
-	// var opa6 =  1-responseXML.panel6 ;
-
-	// var opa7 =  1-responseXML.panel7 ;
-	// var opa8=  1-responseXML.panel8 ;
-
-	// var opa9 =  1- responseXML.panel9 ;
+for {var j = 0; j <responseXML.transcript.length; j++}{
 
 
- // 	document.getElementById("panel1").style.color= rgba(0,0,255,opa1);
- //    document.getElementById("panel2").style.color = rgba(0,0,255,opa2);
- //    document.getElementById("panel3").style.color = rgba(0,0,255,opa3);
- //    document.getElementById("panel4").style.color= rgba(0,0,255,opa4);
- //    document.getElementById("panel5").style.color = rgba(0,0,255,opa5);
- //    document.getElementById("panel6").style.color = rgba(0,0,255,opa6);
- //    document.getElementById("panel7").style.color = rgba(0,0,255,opa7);
- //    document.getElementById("panel8").style.color = rgba(0,0,255,opa8);
- //    document.getElementById("panel9").style.color= rgba(0,0,255,opa9);
 
+
+	if(array.indexOf(responseXML.transcript[j]) > (-1) )
+
+			trans_total += ;
 }  
-
+}
+ document.getElementById("trans_total").value = trans_total;
 
 /*
 
